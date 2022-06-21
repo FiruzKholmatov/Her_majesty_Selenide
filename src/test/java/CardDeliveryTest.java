0264b68dc7598a283cb1cdc04eb4c0b4c52a3da7
firuz kholmatov <firuzkholmatov@yandex.ru>
2022-06-21 17:20:47 +0300

    config added

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,12 +1,6 @@
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -16,45 +10,21 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
-    private WebDriver driver;
-
-    @BeforeAll
-    public static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
     @Test
-    void shouldLoginAccount () {
+    void shouldLoginAccount() {
         Configuration.holdBrowserOpen = true;
+        Configuration.headless = true;
+        Configuration.browser = "chrome";
 
         open("http://localhost:9999/");
 
-       $x("//input[@placeholder='Город']").val("Москва");
-       $x("//input[@placeholder='Дата встречи']").val("24.06.2022");
-       $(By.cssSelector("[data-test-id='name'] input")).val("Бендер Остап");
-       $(By.cssSelector("[data-test-id='phone'] input")).val("+79660000000");
-       $x("//*[@role='presentation']").click();
-       $x("//span[text()='Забронировать']").click();
-       $x("//div[@data-test-id='notification']").should(visible, Duration.ofSeconds(15));
-
-
-
-
-
-
-
-
-
-
+        $x("//input[@placeholder='Город']").val("Москва");
+        $x("//input[@placeholder='Дата встречи']").val("24.06.2022");
+        $(By.cssSelector("[data-test-id='name'] input")).val("Бендер Остап");
+        $(By.cssSelector("[data-test-id='phone'] input")).val("+79660000000");
+        $x("//*[@role='presentation']").click();
+        $x("//span[text()='Забронировать']").click();
+        $x("//div[@data-test-id='notification']").should(visible, Duration.ofSeconds(15));
 
 
     }

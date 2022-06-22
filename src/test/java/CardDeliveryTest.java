019307a88b8277bfea246d957d1400797a0ed9cb
firuz kholmatov <firuzkholmatov@yandex.ru>
2022-06-22 18:47:24 +0300

    worked up

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,8 +1,12 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
@@ -10,22 +14,36 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
+    String planningDate = GenerateDate.generateDate(3);
+
+
     @Test
     void shouldLoginAccount() {
         Configuration.holdBrowserOpen = true;
-        Configuration.headless = true;
+        Configuration.headless = false;
 
 
         open("http://localhost:9999/");
 
         $x("//input[@placeholder='Город']").val("Москва");
-        $x("//input[@placeholder='Дата встречи']").val("24.06.2022");
-        $(By.cssSelector("[data-test-id='name'] input")).val("Бендер Остап");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $(By.cssSelector("[data-test-id='name'] input")).val("Лиходеев Степан");
         $(By.cssSelector("[data-test-id='phone'] input")).val("+79660000000");
         $x("//*[@role='presentation']").click();
         $x("//span[text()='Забронировать']").click();
-        $x("//div[@data-test-id='notification']").should(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
 
     }
+
+    public static class GenerateDate {
+       public static String generateDate (int days) {
+            return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        }
+    }
+
+
 }
